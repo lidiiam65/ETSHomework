f87855d9603d07907317ae6b9f52d24c731c9e27
lidiiam65 <lidiamorozova65@gmail.com>
2021-11-04 17:36:36 +0200

    +inspiration button

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,7 @@
         <v-col>
           <v-btn color="primary" nuxt to="/users"> Users </v-btn>
           <v-btn color="primary" nuxt to="/about"> About </v-btn>
-
-          <!-- <v-btn color="primary" nuxt to="/inspire"> Inspiration </v-btn> -->
+          <v-btn color="primary" nuxt to="/inspire"> Inspiration </v-btn>
         </v-col>
       </v-row>
     </div>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,13 +1,6 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <div>
-        <div>It is inspiration page</div>
-
-        <v-btn color="primary" nuxt to="/"> Homepage </v-btn>
-        <p></p>
-      </div>
-
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
